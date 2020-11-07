094d56b935a41334f258217d922f415ae6dc6f84
Thananon <thananon@digio.co.th>
2020-11-07 11:18:29 +0700

    Init first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for all out flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello world'),
+      ),
+    ),
+  );
+}
+
